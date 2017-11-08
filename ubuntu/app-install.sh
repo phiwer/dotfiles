fdd9f29b3c58d81aea4dfaeb2e378a47969bb178
Philip Werner <philip.werner@phiwer.com>
2017-11-08 18:34:38 +0100

    Added python and pywal to ubuntu apps.

--- ubuntu/app-install.sh
+++ ubuntu/app-install.sh
@@ -51,6 +51,15 @@ sudo apt install arandr
 sudo apt install dh-autoreconf
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
 
+# Python
+sudo apt install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade pip3
+sudo pip install --upgrade virtualenv
+
+# Pywal
+sudo pip3 install pywal
+
 # Java
 sudo apt install openjdk-8-jdk
 
